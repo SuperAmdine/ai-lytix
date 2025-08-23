165baac4f7d316c72fc2379d673476b1a6b7a71d
SuperAmdine <amdine.project.ahc@gmail.com>
2025-08-23 01:05:47 +0100

    05 authentification ui with social

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,16 @@
 import { SigninForm } from "@/components/auth/login.components";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function SigninPage() {
+export default async function SigninPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(), // you need to pass the headers object.
+  });
+  if (!!session) {
+    redirect("/");
+  }
+
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,5 +12,11 @@ export const auth = betterAuth( {
     emailAndPassword: {
         enabled: true,
     },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_LOGIN_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_LOGIN_SECRET as string,
+        },
+    },
 
 } );
